7940ce57e423ddce8774f427d9b6b1d7e2cabec0
haddaoui <chaimahadaoui11@gmail.com>
2025-11-07 15:40:21 +0100

    first commit

---
+++ frontend/app/components/dashboard/Layout.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <main className="p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
